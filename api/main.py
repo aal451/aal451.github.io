42e3ee5660c72b9e819b7ed81cedc86ec644db90
aal451 <aaronlam451@gmail.com>
2024-05-20 12:04:13 -0700

    initial writeup of app

---
+++ api/main.py
@@ -0,0 +1,101 @@
+import uvicorn
+from fastapi import FastAPI
+
+import stanza
+from chinese_english_lookup import Dictionary
+import textwrap
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root() -> None:
+    """
+    Let user know that the only valid API routes are /segment/ and /define/ should they try to call /.
+
+    Parameters:
+    -----------
+        None
+
+    Returns:
+    --------
+        None
+
+    """
+    return {"message": "Invalid Request - to request segmentation, use /segment/(chinese text to segment), to request definitions, use /define/(chinese word to define)"}
+
+
+@app.get("/segment/{text_to_segment}")
+async def segment_input_chinese_into_words(text_to_segment: str) -> list[str]:
+    """
+    Given a string of Chinese text, use the Stanford NLP (natural language processing) Group's Stanza NLP library to segment the text into its individual words, returning the resulting segmentation as a list.
+
+    Parameters:
+    -----------
+        text_to_segment: str
+            A string representing the Chinese text we want to segment into its individual words.
+
+    Returns:
+    --------
+        A list of strings, where each string represents an individual word that made up text_to_segment. Words in the list are in the same order as they appeared in text_to_segment. 
+    """
+
+    # initialize the Stanford NLP Group's Stanza, which will perform the segmentation
+    stanza.download("zh", processors="tokenize")
+
+    # initalize the data pipeline that will ingest the text_to_segment
+    input_pipeline = stanza.Pipeline('zh')
+
+    # Input text_to_segment into the pipeline to generate a Stanza Document object, which contains the segmentation.
+    result_document = input_pipeline(text_to_segment)
+
+    # Extract and return the text's segmentation from the Document.
+    segmentation_result = []
+    for sentence in result_document.sentences:
+        for word in sentence.words:
+            segmentation_result.append(word.text)
+
+    return segmentation_result
+
+
+@app.get("/define/{chinese_word_to_define}")
+async def get_definition_of_chinese_word(chinese_word_to_define: str) -> str:
+    """
+    Given a Chinese word to lookup, return a nicely formatted string representing the definition of the word using definition data from the chinese_english_lookup tool.
+
+    Parameters:
+    -----------
+        chinese_word_to_define: str
+            A string representing the Chinese word to define.
+
+    Returns:
+    --------
+        A string representing chinese_word_to_define's definition, formatted like so:
+
+        Definition:
+          Simplified:  (chinese_word_to_define written in simplified characters)
+          Traditional: (chinese_word_to_define written in traditional characters)
+
+          1) (phonetic (pinyin) representation of the word when the word means this definition) (definition of the word)
+          2) (phonetic (pinyin) representation of the word when the word means this definition) (definition of the word)
+          ... and so on until all available definitions are provided
+    """
+
+    chinese_english_dictionary = Dictionary()
+    dictionary_lookup_result = chinese_english_dictionary.lookup(
+        chinese_word_to_define)
+
+    definition_string_to_return = (
+        f"Definition: \n"
+        + "  Simplified:  {dictionary_lookup_result.simp} \n"
+        + "  Traditional: {dictionary_lookup_result.trad} \n"
+        + "  \n"
+        + textwrap.indent(dictionary_lookup_result.get_definition_entries_formatted(), '  ')
+
+    )
+
+    return definition_string_to_return
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ index.js
@@ -0,0 +1,73 @@
+const segmentButton = document.getElementById("segment-button");
+segmentButton.addEventListener("click", segment);
+
+function segment() {
+    /**
+     * Segment the inputted Chinese text, then display that segmentation in the output area so the user can begin clicking to look up
+     */
+    const chineseInputText = document.getElementById("chinese-input").value;
+
+    // call our custom API to get the segmentation of the text, then display the resulting segmentation in the output area.
+    const apiURL = "https://chinese-tools-api.vercel.app";
+    fetch(apiURL + "/segment/" + chineseInputText)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 500) {
+                    throw new Error("Server Error");
+                }
+                else {
+                    throw new Error("An error occurred while attempting to fetch the segmentation of the input text.")
+                }
+            }
+            return response.json()
+        })
+        .then(segmentationResult => {
+            // add the result of the segmentation to the output area.
+            const segmentationOutputArea = document.getElementById("segmentation-output-area");
+            segmentationResult.foreach(function (wordInSegmentation) {
+                const spanNeededToWrapWord = document.createElement("span")
+                spanNeededToWrapWord.innerText = wordInSegmentation;
+
+                // attach a listener to each word so that when the word is clicked, we can retrieve and display its definition.
+                spanNeededToWrapWord.addEventListener("click", defineWord);
+                segmentationOutputArea.append(spanNeededToWrapWord);
+            });
+
+            // display the result of the segmentation to the user (output area is hidden in the beginning).
+            document.getElementById("segmentation-output-area").style.visibility = "visible";
+        });
+}
+
+function defineWord(event) {
+    /**
+     * Retrieve the definition of the word contained in event.target, then display a popup with that definition above the word for the user to see.
+     */
+    const wordToDefine = event.target.value;
+
+    // call our custom API to get the definition of the word to define, then display the resulting definition in the click-to-lookup popup.
+    const apiURL = "https://chinese-tools-api.vercel.app";
+    let definitionResult = "";
+    fetch(apiURL + "/define/" + wordToDefine)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 500) {
+                    throw new Error("Server Error");
+                }
+                else {
+                    throw new Error("An error occurred while attempting to fetch the segmentation of the input text.")
+                }
+            }
+            definitionResult = response.text();
+        })
+        .then(definitionResult => {
+            // generate and attach the definition popup to the element (in this case, event.target) containing the word.
+            const definitionPopup = bootstrap.getOrCreateInstance(event.target.id, {
+                "content": definitionResult,
+                "trigger": 'focus'
+            });
+
+            // show the definition to the user.
+            definitionPopup.show();
+        });
+}
+
